a0ba025d774195b989eb79824c86acb5b5def62f
Dustin Diaz <polvero@gmail.com>
2015-06-20 22:41:07 -0700

    TESTS!

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,78 @@
-var chai = require('chai')
-  , sinon = require('sinon')
+var request = require('supertest')
+  , express = require('express')
   , redis = require('redis').createClient()
-  , promised = require('chai-as-promised')
   , subject = require('../')
-  , expect = chai.expect
 
-chai.use(promised)
-chai.use(require('sinon-chai'))
+describe('express-it', function () {
 
-describe('rollout', function () {
+  var app, client, getMiddleware
+  beforeEach(function (done) {
+    getMiddleware = function () {
+      return function (req, res, next) {
+        req.variant = 'en.default'
+        next()
+      }
+    }
 
-  it('should work', function () {
-    return expect(true).to.be.true
+    app = express()
+    app.set('view engine', 'jade')
+    app.set('views', __dirname + '/views')
+    client = require('redis').createClient()
+    client.on('connect', done)
+    var VARIANTS = ['en.default', 'en.test']
+    app.use('/path/to/test', subject.init({
+      express: express,
+      redis: client,
+      variants: VARIANTS
+    }))
+
+
+    app.use(getMiddleware())
+
+    app.use(subject.middleware({
+      redis: client,
+      variants: VARIANTS
+    }))
+
+  })
+
+  it('should show backup message when key is unfound', function (done) {
+    app.get('/route', function (req, res) {
+      res.render('test')
+    })
+    request(app)
+    .get('/route')
+    .expect(/example test message/, done)
+  })
+
+  it('should show test message when key is found', function (done) {
+    app.get('/route', function (req, res) {
+      res.render('test', {
+        name: 'earthlings'
+      })
+    })
+
+    request(app)
+    .post('/path/to/test/create')
+    .field('key', 'test.msg')
+    .end(function () {
+      request(app)
+      .put('/path/to/test/update')
+      .send({
+        data: {
+          variant: 'en.default',
+          key: 'test.msg'
+        },
+        value: 'greetings {name}'
+      })
+      .end(function () {
+        request(app)
+        .get('/route')
+        .expect(/greetings earthlings/, done)
+      })
+    })
+
+
   })
 
 })
